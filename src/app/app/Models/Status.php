3cde4f6a92337362e838c7e90e804577db35aa6e
murata0531 <coless115@gmail.com>
2022-08-13 19:16:36 +0900

    Statusモデルを追加

--- src/app/app/Models/Status.php
+++ src/app/app/Models/Status.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function tasks() {
+        return $this->hasOne('App\Models\Task');
+    }
 }
